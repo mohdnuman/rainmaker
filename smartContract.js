18f240ecc9f133aedb2db84eb3796294504c2c9a
mohdnuman <mohammadnuman123@gmail.com>
2022-04-18 23:20:00 +0530

    compressed

--- smartContract.js
+++ smartContract.js
@@ -3,7 +3,6 @@ const Web3 = require("web3");
 const abi = require("./abi.json");
 const abi2 = require("./abi2.json");
 const poolAbi = require("./poolAbi.json");
-const readline = require("readline-sync");
 
 let web3;
 
@@ -12,41 +11,31 @@ const provider = new Web3.providers.HttpProvider(
 );
 web3 = new Web3(provider);
 
-const instance = new web3.eth.Contract(
-  abi,
-  "0x64c2EfCF94129656F1C859E92120252844162513"
-);
-const instance2 = new web3.eth.Contract(
-  abi2,
-  "0xB8681aC978A7D440aaa256e6231CbA99467ccfC4"
-);
 
-async function getBalance() {
-  let address = readline.question("enter address:");
-  console.log("RAIN staking:");
+
+async function getBalance(address) {
+
+  const instance = new web3.eth.Contract(
+    abi,
+    "0x64c2EfCF94129656F1C859E92120252844162513"
+  );
+  const instance2 = new web3.eth.Contract(
+    abi2,
+    "0xB8681aC978A7D440aaa256e6231CbA99467ccfC4"
+  );
+
 
   const decimalsRain = await instance2.methods.decimals().call();
 
-  const walletBalance = await instance.methods.balanceOf(address).call();
-  console.log("wallet balance:", (walletBalance/(10**decimalsRain)).toFixed(2));
-
   const stakedBalance = await instance.methods.getTotalDeposit(address).call();
-  console.log("staked balance:", (stakedBalance/(10**decimalsRain)).toFixed(2));
-
-  const cummalativeRewards = await instance.methods
-    .cumulativeRewardsOf(address)
-    .call();
-  console.log("cummalative rewards:", (cummalativeRewards/(10**decimalsRain)).toFixed(2));
-
-  const withdrawnRewards = await instance.methods
-    .withdrawnRewards(address)
-    .call();
-  console.log("withdrawn rewards:", (withdrawnRewards/(10**decimalsRain)).toFixed(2));
-
   const rewards = await instance.methods.withdrawableRewardsOf(address).call();
-  console.log("staked rewards:", (rewards/(10**decimalsRain)).toFixed(2));
 
-  console.log("RAIN/WETH Pool:");
+  if(stakedBalance!=0){
+  console.log("staked balance:", (stakedBalance/(10**decimalsRain)).toFixed(2));
+  console.log("staked rewards:", (rewards/(10**decimalsRain)).toFixed(2));
+  }
+
+  // RAIN/WETH Pool
   const RAINWETHContract = new web3.eth.Contract(
     poolAbi,
     "0xC4A8C243D8cf7891d6282Bb293E7a0aa4ef1aE51"
@@ -80,7 +69,7 @@ async function getBalance() {
     (LptokensReceivedtoken1 / totalSupplytoken1) * token1Reserve;
   token1amount = token1amount.toFixed(2);
   
-  if ((token0amount != 0, token1amount != 0))
+  if ((token0amount != 0, token1amount != 0)){
     console.log(Symbol0, "+", Symbol1, token0amount, "+", token1amount);
 
   const rewardsWeth = await instance2.methods
@@ -91,7 +80,9 @@ async function getBalance() {
     "staked rewards in Weth pool:",
     (rewardsWeth / 10 ** decimalsRain).toFixed(2)
   );
+  }
 }
 
-getBalance();
+let address="0x20feea68af1618e1ed3dacdbd0ecbe86c5547d62";
+getBalance(address);
 
